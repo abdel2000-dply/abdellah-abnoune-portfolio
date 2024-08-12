3384c08f3306e4070fc2943a08af4dc7e02ff464
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-08-13 00:31:01 +0100

    Update hero section content and image in index.html

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var typed = new Typed('.typing', {
+    strings: ["Software Engineer", "Web Developer", "Full-stack Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+<!-- <span class="word"><!-- <span class="word"></span>
+<span class="word">Web Developer</span>
+<span class="word"></span> -->
+</span>
+          <span class="word">Web Developer</span>
+          <span class="word">Full-stack Developer</span> -->
+        
