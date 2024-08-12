f5914be302f6b914cacc40fbe899d6a01fcaf84f
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-08-13 00:54:48 +0100

    Update main.js to add startDelay to typing effect

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ var typed = new Typed('.typing', {
     strings: ["Software Engineer", "Web Developer", "Full-stack Developer"],
     typeSpeed: 100,
     backSpeed: 60,
+    // startDelay: 1500,
     loop: true
 });
 
