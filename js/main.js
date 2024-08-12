b8f11334e4eb6468fc6509198335a337d54f4b86
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-08-13 00:47:55 +0100

    Update typing effect in main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,9 @@
+// for adding the typing effect to the text
 var typed = new Typed('.typing', {
     strings: ["Software Engineer", "Web Developer", "Full-stack Developer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
 });
-<!-- <span class="word"><!-- <span class="word"></span>
-<span class="word">Web Developer</span>
-<span class="word"></span> -->
-</span>
-          <span class="word">Web Developer</span>
-          <span class="word">Full-stack Developer</span> -->
+
         
