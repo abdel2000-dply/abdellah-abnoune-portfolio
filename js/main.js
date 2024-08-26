18d233cdb299c536c309f8b735528094ae9ef4cf
abdel2000-dply <75086476+abdel2000-dply@users.noreply.github.com>
2024-08-26 22:38:18 +0100

    Refactor skills section to use div elements instead of table

--- js/main.js
+++ js/main.js
@@ -26,21 +26,27 @@ document.addEventListener('DOMContentLoaded', () => {
         <img src="assets/icons/logo-javascript.svg" alt="javascript">
         <img src="assets/icons/typescript.svg" alt="typescript">
         <img src="assets/icons/c-1.svg" alt="c">
+        <img src="assets/icons/bash-2.svg" alt="bash">
+        <img src="assets/icons/html-1.svg" alt="html">
+        <img src="assets/icons/css-3.svg" alt="css">
+        <img src="assets/icons/arduino.svg" alt="arduino">
       </span>
     `,
     frameworks: `
       <span class="icons">
         <img src="assets/icons/react-2.svg" alt="react">
-        <img src="assets/icons/express-109.svg" alt="express">
-        <img src="assets/icons/mongodb.svg" alt="mongodb">
-        <img src="assets/icons/sqlite.svg" alt="sqlite">
+        <img src="assets/icons/expressjs.png" alt="express">
+        <img src="assets/icons/flask.svg" alt="flask">
+        <img src="assets/icons/jquery-1.svg" alt="jquery">
         </span>`,
-        tools: `
+    tools: `
         <span class="icons">
-        <img src="assets/icons/nodejs-icon.svg" alt="nodejs">
+        <img src="assets/icons/nodejs-2.svg" alt="nodejs">
+        <img src="assets/icons/mongodb-icon-2.svg" alt="mongodb">
+        <img src="assets/icons/sqlite.svg" alt="sqlite">
         <img src="assets/icons/git-icon.svg" alt="git">
         <img src="assets/icons/github-icon.svg" alt="github">
-        <img src="assets/icons/vscode-icon.svg" alt="vscode">
+        <img src="assets/icons/visual-studio-code-1.svg" alt="vscode">
         <img src="assets/icons/postman-icon.svg" alt="postman">
       </span>`,
     softskills: `
